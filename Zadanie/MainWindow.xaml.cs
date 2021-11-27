56b6d464144d20912450758ea6008371d6dd774b
Lisek324 <brzozowski2004@gmail.com>
2021-11-27 14:35:07 +0100

    -poprakwa designu -mała zmiana w skrypcie

--- Zadanie/MainWindow.xaml.cs
+++ Zadanie/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
 ﻿/**
  * konieczny warunek na datę
+ * stowrzyć foreacha na comboboxa
+ * warunek czy zdjęcie zostało przesłane
+ * przyciski usuń, import, export jako txt
+ * stworzenie wyszukiwarki
  */
 using System;
 using System.Collections.Generic;
@@ -26,6 +30,56 @@ namespace Zadanie
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Dodaj_onClicik(object sender, RoutedEventArgs e)
+        {
+            /**
+             * u - uczniowe
+             * n - nauczyciele
+             * p - personel
+             * tablice zawierają id kontrolek
+             */
+            TextBox[] formtxtbox_u = {imie_uczen};
+            ComboBox[] formcombobox_u = { };
+            DatePicker[] formdatepicker_u = { };
+
+            TextBox[] formtxtbox_n = { };
+            ComboBox[] formcombobox_n = { };
+            DatePicker[] formdatepicker_n = { };
+
+            TextBox[] formtxtbox_p = { };
+            ComboBox[] formcombobox_p = { };
+            DatePicker[] formdatepicker_p = { };
+
+
+
+            TabItem ti = Tabs.SelectedItem as TabItem;
+
+            MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Dodany zostanie rekord do tabeli " + ti.Header + ", kontynuować?", "Potwierdzenie dodania", System.Windows.MessageBoxButton.YesNo);
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                /**
+                 * w zależności jaka zakładka aktualnie jest wyświetlna, robi się pętla przez tablice, która sprawdzi czy wymagane pola są wypełnione
+                 */
+                switch (ti.Header) {
+                    case "Uczniowie":
+                        foreach (TextBox txt in formtxtbox_u)
+                        {
+                            if (string.IsNullOrEmpty(txt.Text))
+                            {
+                                MessageBox.Show("Nie wypełniono wymaganych pól");
+                                break;
+                            }
+                            else continue;/* to jest źle - tu ma być wpisanie do datagrid*/
+                        }
+                        break;
+                    case "Nauczyciele":
+                        break;
+                    case "Personel":
+                        break;
+                }
+            }
         }
     }
 }
